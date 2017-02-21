f8079ca0bd09d1d18e23815747a980c39c69aea2
Fadly Kayo <fadly.kayo@googlemail.com>
2017-02-21 13:17:07 +0700

    Init skeleton

---
+++ server/controllers/houses.js
@@ -0,0 +1,53 @@
+const Houses = require('../models/houses')
+
+module.exports = {
+  createHouse: (req, res) => {
+    Houses.create({
+      title: req.body.title,
+      specification: req.body.specification,
+      price: req.body.price,
+      location: req.body.location,
+      picture: req.body.picture
+    }).then(function (data) {
+      res.send(data)
+    }).catch(function (err) {
+      res.send(err)
+    })
+  },
+
+  getHouses: (req, res) => {
+    Houses.find().then(function (data) {
+      res.send(data)
+    }).catch(function (err) {
+      res.send(err)
+    })
+  },
+
+  updateHouse: (req, res) => {
+    Houses.findOneAndUpdate({
+      _id: req.params.id
+    }, {
+      title: req.body.title,
+      specification: req.body.specification,
+      price: req.body.price,
+      location: req.body.location,
+      picture: req.body.picture
+    }, {
+      new: true
+    }).then(function (data) {
+      res.send(data)
+    }).catch(function (err) {
+      res.send(err)
+    })
+  },
+
+  deleteHouse: (req, res) => {
+    Houses.findOneAndRemove({
+      _id: req.params.id
+    }).then(function () {
+      res.send({message: `Deleted House with ID: ${req.params.id}`})
+    }).catch(function (err) {
+      res.send({message: 'Error data not found'})
+    })
+  }
+}
---
+++ server/models/houses.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const HouseSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  specification: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  picture: {
+    type: String
+  }
+},
+  {
+    timestamps: true
+  })
+
+module.exports = mongoose.model('Houses', HouseSchema)
